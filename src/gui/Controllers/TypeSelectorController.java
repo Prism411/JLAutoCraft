394bad90fd876979ecdbc6683cef4a944756a302
Jáder Louis <jaderlouis3@gmail.com>
2023-01-29 20:58:52 -0300

    working on TypeSelector

--- src/gui/Controllers/TypeSelectorController.java
+++ src/gui/Controllers/TypeSelectorController.java
@@ -1,5 +1,22 @@
 package gui.Controllers;
 
-public class TypeSelectorController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import entities.PlaceType;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+public class TypeSelectorController implements Initializable {
+	
+	@FXML
+	private ComboBox<PlaceType> comboBox;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		comboBox.getItems().addAll(PlaceType.values());
+		
+	}
 
 }
